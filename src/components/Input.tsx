7a5b0ed47d122bcd2f38e1b79192661ed7087295
Anton Palermo <ANPBusinessmail@gmail.com>
2021-10-11 12:12:55 +0800

    move to todo-app dir

--- src/components/Input.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Input = () => {
-  return <input type={'text'} autoComplete={'off'} />
-}
-
-export default Input
--- src/entity/Task.ts
+++
@@ -1,4 +0,0 @@
-export default class Task {
-  name!: string
-  isCompleted: boolean = false
-}
