d5a44f1fa1b1e07f6bf906dc021f38cfbc3cd81b
Anton Palermo <ANPBusinessmail@gmail.com>
2021-10-23 07:23:43 +0800

    added placeholders

--- tiptap-editor/components/Tiptap.tsx
+++ tiptap-editor/components/Tiptap.tsx
@@ -1,9 +1,34 @@
+import Document from '@tiptap/extension-document'
 import StarterKit from '@tiptap/starter-kit'
+import Placeholder from '@tiptap/extension-placeholder'
 import { useEditor, EditorContent } from '@tiptap/react'
+
+const customDocument = Document.extend({
+  content: 'heading block*',
+})
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    editorProps: {
+      attributes: {
+        class:
+          'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
+      },
+    },
+    extensions: [
+      customDocument,
+      StarterKit.configure({
+        document: false,
+      }),
+      Placeholder.configure({
+        placeholder: ({ node }) => {
+          if (node.type.name === 'heading') {
+            return 'Untitled Document'
+          }
+          return 'Write something ...'
+        },
+      }),
+    ],
   })
 
   return <EditorContent editor={editor} />
