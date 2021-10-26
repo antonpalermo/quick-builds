33490dd2ba046731d6839a24ba1ed99ce62dba13
Anton Palermo <ANPBusinessmail@gmail.com>
2021-10-26 21:49:46 +0800

    added custom doc

--- tiptap-editor/components/Tiptap.tsx
+++ tiptap-editor/components/Tiptap.tsx
@@ -12,7 +12,7 @@ const Tiptap = () => {
     editorProps: {
       attributes: {
         class:
-          'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
+          'prose prose-md sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
       },
     },
     extensions: [
